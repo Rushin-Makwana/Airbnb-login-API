89e9d3f76e79262a71d2964f450d09f7305d9779
Rushin-Makwana <rushymakwana@gmail.com>
2022-08-23 02:27:26 -0700

    made

--- app.js
+++ app.js
@@ -1,48 +1,54 @@
-let express = require('express')
-let app = express();
-let bodyparser = require('body-parser')
-let superagent = require('superagent')
-let port = 9700;
-let cors = require('cors');
-let request = require('request')
-app.use(bodyparser.urlencoded({extended:true}));
-app.use(bodyparser.json())
+const express = require('express')
+const app = express();
+const superagent = require('superagent');
+const request = require('request');
+const bodyParser = require('body-parser');
+const port = 9800;
+const cors = require('cors')
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json())
 app.use(cors());
 
-app.get('/', (req,res)=>{
-    res.send('<a href="https://github.com/login/oauth/authorize?client_id=01ccaee9766fd61430b8">Login With Git </a>')
+app.get('/',(req,res) => {
+    res.send('<a href="https://github.com/login/oauth/authorize?client_id=a95b1d520a747c9b9223">Login With Github</a>')
 })
 
-app.get('/profile',(req,res)=>{
+app.post('/oauth',(req,res) => {
     console.log(req.body)
-    const code = req.query.code
+    const code = req.body.code;
     if(!code){
-        res.send('Invalid Login')
+        res.send('Login Fail')
     }
-    superagent.post('https://github.com/login/oauth/access_token')
-    .send({
-        client_id:'01ccaee9766fd61430b8',
-        client_secret: '486a513d4a2083487632b53a20ba790f3fc32c08',
-        code: code
-    })
-    .set('Accept','application/json')
-    .end((err, result)=>{
-        if(err) throw err;
-        let access_token = result.body.access_token
-        const option ={
-            uri: 'https://api.github.com/user',
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Authorization': `token ${access_token}`,
-                'User-Agent' :'mycode'
-            }
-        }
-        request(option, (err,response, body)=>{
-            return res.send(body)
+
+    superagent
+        .post('https://github.com/login/oauth/access_token')
+        .send({
+            client_id:'a95b1d520a747c9b9223',
+            client_secret:'13acb984a62ac64e6394a380f08e63f9109c5bf2',
+            code:code
         })
-    })
+        .set('Accept','application/json')
+        .end((err,result) => {
+            if(err) throw err;
+            let access_token = result.body.access_token
+            const option = {
+                uri: 'https://api.github.com/user',
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Authorization': `token ${access_token}`,
+                    'User-Agent':'mycode'
+                }
+            }
+            request(option,(err,response,body) => {
+                console.log(body)
+                return res.send(body)
+                
+            })
+        })
 })
-app.listen(port, ()=>{
-    console.log(`Server is listening to post {port}`)
+
+app.listen(port,()=>{
+    console.log(`Server is listening to post ${port}`)
 })
